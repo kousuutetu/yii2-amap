c5c16fd17bc3fdd6f454431bf410da51e0ca8eff
kousuutetu <kousuutetu@163.com>
2018-10-20 15:27:43 +0800

    add search

--- Yuntu.php
+++ Yuntu.php
@@ -5,6 +5,7 @@ namespace ginkgo\amap;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
 use yii\httpclient\Client;
 use yii\web\HttpException;
 
@@ -25,26 +26,34 @@ class Yuntu extends \yii\base\Component
 
     public function create($name, $location, $options=[])
     {
-        return $this->request('create', ['data' => Json::encode(ArrayHelper::merge([
+        return $this->datamanage('create', ['data' => Json::encode(ArrayHelper::merge([
             '_name' => $name,
             '_location' => $location,
-        ], $options)));
+        ], $options))]);
     }
 
     public function update($id, $options=[])
     {
-        return $this->request('update', ['data' => Json::encode(ArrayHelper::merge([
+        return $this->datamanage('update', ['data' => Json::encode(ArrayHelper::merge([
             '_id' => $id,
-        ], $options)));
+        ], $options))]);
     }
 
     public function delete($ids)
     {
         $ids = is_array($ids) ? implode(',', $ids) : $ids;
-        return $this->request('delete', ['ids' => $ids]);
+        return $this->datamanage('delete', ['ids' => $ids]);
     }
 
-    public function request($uri, $data)
+    public function around($center, $options)
+    {
+        $data = ArrayHelper::merge([
+            'center' => $center,
+        ], $options);
+        return $this->datasearch('around', $data);
+    }
+
+    public function datamanage($action, $data)
     {
         $data = ArrayHelper::merge([
             'key'  => $this->key,
@@ -53,7 +62,7 @@ class Yuntu extends \yii\base\Component
 
         $client = new Client(['baseUrl' => 'http://yuntuapi.amap.com/datamanage/data/']);
         $response = $client->createRequest()
-            ->setUrl($uri)
+            ->setUrl($action)
             ->setMethod('post')
             ->addHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])
             ->setData($data)
@@ -68,5 +77,33 @@ class Yuntu extends \yii\base\Component
         }
 
         return $response->data;
+    }
+
+    public function datasearch($action, $data)
+    {
+        $data = ArrayHelper::merge([
+            'key'  => $this->key,
+            'tableid' => $this->tableid,
+        ], $data);
+
+        $client = new Client(['baseUrl' => 'https://yuntuapi.amap.com/datasearch/']);
+        $response = $client->createRequest()
+            ->setUrl($action)
+            ->setMethod('get')
+            ->setData($data)
+            ->send();
+
+        if (!$response->isOk) {
+            return false;
+        }
+
+        if (!$response->data['status']) {
+            return [];
+        }
+
+        return [
+            'count' => $response->data['count'],
+            'datas' => $response->data['datas'],
+        ];
     }
 }
